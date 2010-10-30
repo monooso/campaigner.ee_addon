1b283e8a1d183e0248d095065a1e0c45b097872c
Stephen Lewis <stephen@experienceinternet.co.uk>
2010-10-30 23:05:51 +0100

    Update mock Campaigner Model to reflect recent changes.

--- third_party/campaigner/tests/mocks/mock.campaigner_model.php
+++ third_party/campaigner/tests/mocks/mock.campaigner_model.php
@@ -16,30 +16,34 @@ class Mock_campaigner_model {
 	 * ------------------------------------------------------------ */
 	
 	public function activate_extension() {}
+	public function activate_extension_mailing_lists_table() {}
+	public function activate_extension_register_hooks() {}
+	public function activate_extension_settings_table() {}
 	public function disable_extension() {}
-	public function fix_api_response($api_result = array(), $root_node) {}
 	public function get_clients_from_api() {}
-	public function get_custom_fields_from_input_array($custom_fields_data = array()) {}
 	public function get_extension_class() {}
 	public function get_extension_settings() {}
 	public function get_installed_extension_version() {}
+	public function get_mailing_list_custom_fields_from_api() {}
 	public function get_mailing_lists_from_api() {}
 	public function get_mailing_lists_from_db() {}
-	public function get_mailing_lists_from_input() {}
 	public function get_member_fields() {}
 	public function get_package_name() {}
 	public function get_package_version() {}
 	public function get_settings_from_db() {}
 	public function get_site_id() {}
+	public function get_support_url() {}
 	public function get_theme_url() {}
+	public function make_api_call($method, $params = array(), $root_node) {}
+	public function prep_api_response($api_result, $root_node) {}
 	public function save_extension_settings($settings) {}
 	public function save_mailing_lists_to_db($settings) {}
 	public function save_settings_to_db($settings) {}
 	public function set_api_connector($api_connector) {}
+	public function update_basic_settings_from_input($settings) {}
 	public function update_extension($installed_version = '', $package_version = '') {}
 	public function update_extension_settings_from_input($settings) {}
-	public function update_settings_from_input($settings) {}
-	public function validate_api_response($api_response = array(), $root_node = '') {}
+	public function update_mailing_list_settings_from_input($settings) {}
 	
 }
 
