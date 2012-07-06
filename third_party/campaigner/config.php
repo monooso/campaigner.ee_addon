16b97dcad0f97a96777c1a380a9dee209f4cbae7
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-07-06 12:33:50 +0100

    Bump version number.

--- third_party/campaigner/config.php
+++ third_party/campaigner/config.php
@@ -6,13 +6,13 @@
  * @author          Stephen Lewis (http://github.com/experience/)
  * @copyright       Experience Internet
  * @package         Campaigner
- * @version         4.5.0b1
+ * @version         4.5.0
  */
 
 if ( ! defined('CAMPAIGNER_NAME'))
 {
   define('CAMPAIGNER_NAME', 'Campaigner');
-  define('CAMPAIGNER_VERSION', '4.5.0b1');
+  define('CAMPAIGNER_VERSION', '4.5.0');
 }
 
 $config['name']     = CAMPAIGNER_NAME;
